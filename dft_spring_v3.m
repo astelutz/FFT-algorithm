15915b27f691219083f40b1bfbb39e72c92867c2
astelutz <andrewstevlutz@outlook.com>
2025-04-04 11:30:57 -0400

    Same as prior, forgot the updated code.

--- dft_spring_v3.m
+++ dft_spring_v3.m
@@ -2,17 +2,18 @@
 %% Initialization
 close all
 clear,clc
-file = ".txt"; % grabs our data file
+file = "fourier_dataset_2.txt"; % grabs our data file
 data = readmatrix(file); % converts our data file into a matrix
 
 if mod(size(data,1),2) == 1
     N = size(data,1)-1; % determines the size of our matrices
+    T = data(N+1); % picks the period from the final value in our time column
 else
     N = size(data,1);
+    T = data(N);
 end
 t = data(1:N,1); % stores the first column (time) as a column vector
 f = data(1:N,2); % stores the second column (f(t)) as a column vector
-T = data(N+1); % picks the period from the final value in our time column
 dt = abs(t(1)-t(2)); % determines our delta t from our time matrix
 M = 2; % number of wave modes to sweep for
 qmax = floor((N-1)/2);
@@ -25,18 +26,19 @@ inv(N,qmax) = zeros;
 %Optional Randomness
 %{
 for j = 1:N
-    f(j) = f(j) + 0.2*rand;
+    %f(j) = f(j) + 0.2*rand;
+    %f(j) = f(j) + 2*cos(3*2*pi*j*dt);
 end
 %}
-
 f = f - mean(f);
 %% Fourier Transform
 for q = 1:qmax
-    for n = 1:N
+    for n = 1:N    
         A(q) = (2/N) * f(n) * exp((1i*2*pi*q*(n-1))/N) + A(q);
         C(q) = (2/N) * f(n) * cos((2*pi*q*(n-1))/N) + C(q);
         D(q) = (2/N) * f(n) * sin((2*pi*q*(n-1))/N) + D(q);
-        Mag(q) = sqrt((C(q))^2 + (D(q))^2);
+        %Mag(q) = sqrt((C(q))^2 + (D(q))^2);
+        Mag(q) = abs(A(q));
     end
     for n = 1:N
         inv(n,q) = C(q) * cos(2*pi*q*(n-1)/N) + D(q) * sin(2*pi*q*(n-1)/N);
@@ -45,6 +47,7 @@ end
 F = sum(inv,2);
 
 %% Results
+%
 figure(1)
 plot((1/T):(1/T):(qmax/T),C)
 hold on
@@ -53,17 +56,22 @@ hold off
 legend('C Coefficient','D Coefficient')
 ylabel('Strength of Coefficient')
 xlabel('idk yet')
-
+%
+x_ticks=0:0.5:qmax;
+x_label{floor((qmax+1)*dt),1}=zeros;
+x_label{1} = '0';
+for j = 2:2*floor((qmax+1)*dt)
+    x_label{j} = sprintf('%d\\pi',j-1);
+end
+xticklabels(x_label);
+%}
 figure(2)
 plot((1/T):(1/T):(qmax/T),Mag)
 ylabel('Power')
 xlabel('Frequency (kind of)')
-%{
-xticks(0:(pi/2):ceil(qmax/T)*(2*pi))
-for j = 0:size(0:(pi/2):ceil(qmax/T)*(2*pi),2) - 1
-xticklabels('\pi')
-end
-%}
+xticks(x_ticks)
+xticklabels(x_label);
+%
 figure(3)
 plot(t,f)
 hold on
@@ -71,3 +79,4 @@ plot(t,F)
 legend('f(t)','Inv(F(k))')
 ylabel('Amplitude (arbitrary units)')
 xlabel('Time (s)')
+%}
